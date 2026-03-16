8e6e83431b1d0307a38fa88e1752c75863089fdf
thaiphamhuonggiang5148-cyber <thaiphamhuonggiang5148@gmail.com>
2026-03-16 11:54:50 +0700

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
-
 import Login from './components/Login'; 
+import Register from './components/Register'; 
+import ForgotPassword from './components/ForgotPassword';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('login'); 
+
   return (
     <div className="App">
-      <Login />
+      {currentPage === 'login' && (
+        <Login 
+          onSwitchRegister={() => setCurrentPage('register')} 
+          onSwitchForgot={() => setCurrentPage('forgot')} 
+        />
+      )}
+      
+      {currentPage === 'register' && (
+        <Register onSwitchLogin={() => setCurrentPage('login')} />
+      )}
+      
+      {currentPage === 'forgot' && (
+        <ForgotPassword onSwitchLogin={() => setCurrentPage('login')} />
+      )}
     </div>
   );
 }
